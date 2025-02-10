375f4d62c66385af097b850d08d623cd1067a4c5
Walla-42 <walla42.byu@gmail.com>
2025-02-09 21:43:07 -0700

    Started adding to the report class

--- main.py
+++ main.py
@@ -71,6 +71,42 @@ class UserSettings():
     def save_preferences():
         pass
 
-class Report():
-    def __init__():
+class Report(Server):
+    def __init__(self, database_name='transactions.db', category:str=None, date_range:tuple=None, store:str=None):
+        super().__init__(database_name)
+
+        base_query = "SELECT * FROM expenses WHERE 1=1"
+        if category:
+            base_query += f" AND category = {category}"
+        if date_range:
+            base_query += f" AND date BETWEEN {date_range[0]} AND {date_range[1]}"
+        if store:
+            base_query += f" AND store  = {store}"
+        self.expenses = self.c.execute(base_query)
+        self.income = self.c.execute("SELECT * FROM income")
+        self.expense_data = self.expenses.fetchall()
+        self.income_data = self.income.fetchall()
+
+
+    def percent_income_spent(self):
+        total_income = [sum(income[2] for income in self.income_data)]
+        total_expense = [sum(expense[3] for expense in self.expense_data)]
+        return total_expense/total_income * 100
+    
+    def spending_breakdown(self):
+        pass
+
+    def highest_expense(self):
+        pass
+
+    def avg_daily_expense(self):
+        pass
+
+    def avg_categorial_expense(self):
+        pass
+
+    def spending_trend(self):
+        pass
+
+    def generate_report(self):
         pass
